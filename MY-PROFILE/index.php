3b92ac5ae62f6155de5bb761b0f19738bcd0f076
DevaanshuSingh <devaanshu111@gmail.com>
2025-04-05 18:12:31 +0530

    Some More,

--- MY-PROFILE/index.php
+++ MY-PROFILE/index.php
@@ -14,7 +14,7 @@ $stmt->execute([]);
 $allFeatures = $stmt->fetchAll(PDO::ATTR_AUTOCOMMIT);
 
 if ($me) {
-    print_r($me);
+    // print_r($me);
 ?>
     <!DOCTYPE html>
     <html lang="en">
@@ -25,6 +25,7 @@ if ($me) {
         <title>My Profile</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" rel="stylesheet" />
+        <link rel="shortcut icon" href="../MEDIA/favicon.ico" type="image/x-icon">
 
         <link rel="stylesheet" href="style.css">
     </head>
